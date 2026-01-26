b9720e959ddf715cb579f92538215129916df5fc
Vaibhavsable451 <vaibhavsable451@gmail.com>
2026-01-26 11:35:15 +0530

    Remove redundant @CrossOrigin from controllers

--- backend/src/main/java/com/vaibhav/controller/AuthController.java
+++ backend/src/main/java/com/vaibhav/controller/AuthController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
-@CrossOrigin
 public class AuthController {
 
     private final UserRepository userRepository;
