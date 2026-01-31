d5536d41b5c767d380ebba3f219e77afc7acf399
Vaibhavsable451 <vaibhavsable451@gmail.com>
2026-01-31 14:37:37 +0530

    Fix build error: Add missing CrossOrigin import in AuthController

--- src/main/java/com/vaibhav/Controller/AuthController.java
+++ src/main/java/com/vaibhav/Controller/AuthController.java
@@ -12,6 +12,7 @@ import com.vaibhav.service.AuthService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
