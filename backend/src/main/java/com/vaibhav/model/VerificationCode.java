ce87e698465fb48dcbeb5cadb0db87f49c4895c1
Vaibhav Sable <118437085+Vaibhavsable451@users.noreply.github.com>
2026-02-21 12:03:04 +0530

    Update VerificationCode.java

--- backend/src/main/java/com/vaibhav/model/VerificationCode.java
+++ backend/src/main/java/com/vaibhav/model/VerificationCode.java
@@ -11,7 +11,7 @@ import lombok.*;
 @EqualsAndHashCode
 public class VerificationCode {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.Auto)
     private Long id;
 
     private  String otp;
